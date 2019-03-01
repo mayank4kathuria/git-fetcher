2924110783b586fb54370e86c20c865bb0d8ca4c
hoodie Coder <mayank4kathuria@gmail.com>
2019-03-01 08:34:00 +0530

    Api module updated

--- src/api.js
+++ src/api.js
@@ -1,19 +1,20 @@
-// A Seprate Module for API calls
+
+// An Individual Module for API calls
 
 const URL = "https://api.github.com/users/";
 
-
+// API call to fetch User details
 export const fetchUser = (user) => (
     fetch(`${URL}${user}`)
     .then( res => res.json())
     .then( val => val)
-    .catch( err => console.log("err is",err))
+    .catch( err => [])
 )
 
+// API call to fetch User Repo
 export const fetchRepo = (repos) => (
   fetch(repos)
   .then(res => res.json())
   .then(val => val)
-  .catch( err => console.log("err in repo is",err))
-  )
-
+  .catch( err => [])
+)
